43a94bb3b749abd84722243206f32b6458b2eaf3
Silas Hsu <hsu.silas@wustl.edu>
2017-12-01 18:40:20 -0600

    Added more config lines

--- HubConfig.py
+++ HubConfig.py
@@ -7,10 +7,12 @@ class Species(Enum): # Each species will be written to a hub file called "$speci
 
 ignore = [ # Property keys that match these strings will be ignored.
     "_Other_MiSeq",
-    "RPE1",
     "3PNAS_2016_3Rhesus",
     "5biorxiv_2017",
     "4Science_2017",
+    "Drosophila",
+    "External_rowley2017", # Contains 5 Drosophila tracks, among others
+    "External_2014Feng", # Arabidopsis.  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4347903/
 ]
 
 SampleInfo = collections.namedtuple("SampleInfo", "id species")
@@ -29,7 +31,10 @@ sample_lookup = { # The script will look in the input's keys and fields for a me
     "GM06990": SampleInfo("13076", Species.HUMAN),
     "RWPE1":   SampleInfo("13293", Species.HUMAN),
     "NB4":     SampleInfo("14008", Species.HUMAN),
-
+    # FIXME so RPE1 cells are Human Retinal Pigment Epithelial Cells, similar to "HRPEpiC" cells, but they are not
+    # HRPEpiC.  So I've just specified them as eye cells.
+    "RPE1":    SampleInfo("15037", Species.HUMAN),
+    "HEK293T": SampleInfo("12220", Species.HUMAN),
 
     "Patski":                   SampleInfo("10007", Species.MOUSE),
     "CH12-LX":                  SampleInfo("10003", Species.MOUSE),
@@ -39,9 +44,9 @@ sample_lookup = { # The script will look in the input's keys and fields for a me
     "mNSC":                     SampleInfo("10084", Species.MOUSE),
     "mAST":                     SampleInfo("10085", Species.MOUSE),
     "2013Seitan_Tcell_Rad21":   SampleInfo("10086", Species.MOUSE),
-
-    #"RPE1": "????",
-    #"AaegL4": "????",
+    "Mouse brain":              SampleInfo("10019", Species.MOUSE),
+    "2013Nagano_cell":          SampleInfo("10080", Species.MOUSE),
+    "External_2012Zhang":       SampleInfo("10050", Species.MOUSE)
 }
 
 species_to_custom_vocab = { # Custom props for each species' "vocabulary"
